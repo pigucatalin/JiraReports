28545fd1bd6356b553f6010131dad27166489554
Catalin Comanici <ccomanici@optaros>
2015-03-12 19:54:04 +0200

    primu comit

---
+++ daysBugsReportedCorrectly.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Httpful;
+include_once "./vendor/nategood/httpful/src/Httpful/Request.php";
+include_once "./vendor/nategood/httpful/src/Httpful/Http.php";
+include_once "./vendor/nategood/httpful/src/Httpful/Bootstrap.php";
+
+
+ini_set('display_errors', '0');
+
+$ini_array = parse_ini_file("jira.properties");
+
+$username = $ini_array['user'];
+$pass = $ini_array['pass'];
+
+$yesterday = date('Y-m-d',strtotime("-1 days"));
+$jqlStr = urlencode('project = e-Replatform AND type = Bug and status not in (Closed,"Customer Testing", Resolved) AND createdDate >='.$yesterday);
+$uri = "https://newpig.atlassian.net/rest/api/2/search?jql=".$jqlStr."&maxResults=1000";
+$response = Request::get($uri)->authenticateWithBasic($username, $pass)->send();
+
+$issueCounter=0;
+while($response->body->issues[$issueCounter]){
+	$issueResp = Request::get($response->body->issues[$issueCounter]->self)->authenticateWithBasic($username, $pass)->send();
+
+	echo "----------------------\n";
+	echo $issueResp->body->key."\n";
+
+	$allGood = checkPriority($issueResp) & hasStory($issueResp) & hasEpic($issueResp) & hasSeverity($issueResp) & checkEnvironment($issueResp);
+
+	if($allGood){
+		echo "ALL GOOOD!!!!\n";
+	}
+
+	echo "----------------------\n";
+
+	$issueCounter++;
+}
+
+
+
+function checkPriority($issueResp){
+	$prioName = $issueResp->body->fields->priority->name;
+	$acceptedValues = array("Blocker", "Critical", "Medium", "Trivial");
+	if(!in_array($prioName, $acceptedValues, false)){
+		echo "Priority set to: ".$prioName."\n";
+		return false;
+	}
+	return true;
+}
+
+function hasStory($issueResp){
+	$count = 0;
+	while(isset($issueResp->body->fields->issuelinks[$count])){
+		if($issueResp->body->fields->issuelinks[$count]->outwardIssue->fields->issuetype->name == "Story"){
+			return true;
+		}
+		$count++;
+	}
+	echo "NO STORY LINKED!!\n";
+	return false;
+}
+
+function hasEpic($issueResp){
+	if(isset($issueResp->body->fields->customfield_10008)){
+		return true;
+	}
+	echo "NO EPIC SET!!!";
+	return false;
+}
+
+function hasSeverity($issueResp){
+	$setSev = $issueResp->body->fields->customfield_11700->value;
+	$severities = array("Sev1", "Sev2", "Sev3");
+
+	if(in_array($setSev, $severities, false)){
+		return true;
+	}
+	echo "Severity set to: ".$setSev."\n";
+}
+
+function checkEnvironment($issueResp){
+	$setEnv = $issueResp->body->fields->environment;
+	$contains = array();
+	array_push($contains, preg_match('/Env:/', $setEnv));
+	array_push($contains, preg_match('/Build:/', $setEnv));
+	array_push($contains, preg_match('/Browser:/', $setEnv));
+	array_push($contains, preg_match('/Mobile:/', $setEnv));
+
+	foreach($contains as $var){
+		if(!$var){
+			echo "Environment set to: ".$setEnv."\n";
+			return false;
+		}
+	}
+	return true;
+}
+
+
+
+
+
+
+/**
+ *
+ * $issueResp->body->key
+ * $issueResp->body->fields
+ * $issueResp->body->fields->priority->name 													?? e in alea 4 ?
+ * $issueResp->body->fields->issuelinks[$i]->outwardIssue->fields->issuetype->name				?? e vreuna de type story
+ * $issueResp->body->fields->customfield_10008     								?? is epicLink set ?
+ * $issueResp->body->fields->customfield_11700->value     								?? severity value ?
+ *
+ * $issueResp->body->fields->environment     								?? contains the words to BUILD ..... ?
+ *
+ * https://newpig.atlassian.net/rest/api/2/issue/26511
+ *
+ * $response->body->issues[0]
+ * $response->body->issues[0]->self
+ * $response->body->issues[0]->key
+ *
+ * $response->body->issues[0]->key->fields->priority->name ?? e in alea 4 ?
+ *
+ * $response->body->issues[0]->key->fields->issuelinks[i]->outwardIssue->fields->issuetype->name ???= Story
+ *		$response->body->issues[0]->key->fields->environment  ???? contains words ENV, BUILD ... ?
+ */
---
+++ hoursInSprint.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Httpful;
+include_once "./vendor/nategood/httpful/src/Httpful/Request.php";
+include_once "./vendor/nategood/httpful/src/Httpful/Http.php";
+include_once "./vendor/nategood/httpful/src/Httpful/Bootstrap.php";
+
+
+//ini_set('display_errors', '0');
+
+$ini_array = parse_ini_file("jira.properties");
+$username = $ini_array['user'];
+$pass = $ini_array['pass'];
+
+
+$jqlStr = urlencode('project = e-Replatform AND type = "QA Task" AND Sprint = 136 and status not in (Closed,"Customer Testing", Resolved)');
+//$jqlStr = urlencode('key = ER-1683');
+$uri = "https://newpig.atlassian.net/rest/api/2/search?maxResults=1000&jql=".$jqlStr;
+$response = Request::get($uri)->authenticateWithBasic($username, $pass)->send();
+
+//print_r($response->body);
+
+/*
+ * $response->body->issues[0]->key
+ * $response->body->issues[0]->timeestimate
+ * $response->body->issues[0]->fields->aggregatetimeestimate
+ * $response->body->issues[0]->fields->assignee->name
+ *
+ */
+
+
+$issueCounter=0;
+$assignedTimes = array();
+while($response->body->issues[$issueCounter]){
+	$issue = $response->body->issues[$issueCounter];
+	$assignedTimes[$issue->fields->assignee->name] += $issue->fields->aggregatetimeestimate;
+	$issueCounter++;
+}
+
+foreach(array_keys($assignedTimes) as $key){
+	echo "$key -- ". $assignedTimes[$key]/3600 . "\n";
+}
